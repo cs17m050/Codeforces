0bb6297874925a8444b0f95ac110608c92e0ea52
cs17m050 <37400040+cs17m050@users.noreply.github.com>
2018-03-28 18:21:42 +0530

    Update kmp array.cpp

--- kmp array.cpp
+++ kmp array.cpp
@@ -1,18 +1,24 @@
-#include <iostream>
-#include <cstdio>
-#include <map>
-#include <algorithm>
-using  namespace  std;
-typedef long long int ll;
+/*
+s is the pattern string to be searched.
+a is the preprocessor array.
+a.size() should be >= s.size()
+Example:
+if s = "ababc"
+then,
+a = {-1, -1, 0, 1, -1}
+*/
 void construct_kmp_array(int a[],char s[])
 {
   int i,j;
   a[0]=-1;
-  for(i=1;s[i]!='\0';i++){
+  for(i=1;s[i]!='\0';i++)
+  {
     a[i]=-1;
     j=i-1;
-    while(j>=0){
-      if(s[a[j]+1]==s[i]){
+    while(j>=0)
+    {
+      if(s[a[j]+1]==s[i])
+      {
 	a[i]=a[j]+1;
 	break;
       }
@@ -20,15 +26,4 @@ void construct_kmp_array(int a[],char s[])
 	j=a[j];
     }
   }
-}
-int main(){
-  int a[100],i;
-  char s[100];
-  scanf("%s",s);
-  
-  construct_kmp_array(a,s);
-  for(i=0;i<20;i++){
-    printf("%d ",a[i]);
-  }
-  return 0;
 }
